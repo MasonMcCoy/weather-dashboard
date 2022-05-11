894371927d49cfeb3387ac970612f430314f3d68
Mason McCoy <mmasonmccoy@gmail.com>
2022-05-11 18:25:44 -0500

    Search button now logs all current weather conditions for a given city on click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,48 @@
 var key = "cc82ba65ca932ac8748f141a8908c5f9";
-var baseURL = "https://api.openweathermap.org/data/2.5/weather";
-var city = "Chicago";
 
-var call = baseURL + "?q=" + city + "&appid=" + key;
+var srchBtn = $("#search-bttn");
 
-function getWeather(city) {
+function getCoor() {
+    var city = $("#search-input").val();
+
+    var baseURL = "https://api.openweathermap.org/data/2.5/weather";
+    var call = baseURL + "?q=" + city + "&appid=" + key + "&units=imperial";
+
     $.ajax({url: call, success: function(response) {
-        console.log(response);
+        // City Name
+        console.log(response.name);
+
+        // Date
+        console.log(response.dt);
+
+        getWeather(response.coord);
     }})
 }
+
+function getWeather(coordinates) {
+    var baseURL = "https://api.openweathermap.org/data/2.5/onecall";
+    var lat = coordinates.lat;
+    var lon = coordinates.lon;
+    var call =  baseURL + "?lat=" + lat + "&lon=" + lon +
+    "&exclude=minutely,hourly,daily,alerts&appid=" + key;
+
+    $.ajax({url: call, success: function(response) {
+        // Weather Icon
+        console.log(response.current.weather[0].icon);
+
+        // Temperature
+        console.log(response.current.temp);
+        
+        // Humidity
+        console.log(response.current.humidity);
+        
+        // Wind Speed
+        console.log(response.current.wind_speed);
+        
+        // UV Index
+        console.log(response.current.uvi);
+    }})
+
+}
+
+srchBtn.on("click", getCoor);
