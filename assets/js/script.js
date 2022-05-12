01f5539f2ada855725f0a767d0a34b93d3435a1d
Mason McCoy <mmasonmccoy@gmail.com>
2022-05-11 19:15:20 -0500

    Cleaned up API calls and added fucntion to convert unix timestamp.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,33 @@
 var key = "cc82ba65ca932ac8748f141a8908c5f9";
 
 var srchBtn = $("#search-bttn");
+var weatherInfo = $("main");
 
 function getCoor() {
     var city = $("#search-input").val();
 
     var baseURL = "https://api.openweathermap.org/data/2.5/weather";
-    var call = baseURL + "?q=" + city + "&appid=" + key + "&units=imperial";
+    var call = baseURL + "?q=" + city + "&appid=" + key;
 
     $.ajax({url: call, success: function(response) {
-        // City Name
-        console.log(response.name);
-
-        // Date
-        console.log(response.dt);
-
-        getWeather(response.coord);
+        getWeather(response.coord, response.name);
     }})
 }
 
-function getWeather(coordinates) {
+function getWeather(coordinates, city) {
     var baseURL = "https://api.openweathermap.org/data/2.5/onecall";
     var lat = coordinates.lat;
     var lon = coordinates.lon;
     var call =  baseURL + "?lat=" + lat + "&lon=" + lon +
-    "&exclude=minutely,hourly,daily,alerts&appid=" + key;
+    "&exclude=minutely,hourly,daily,alerts&appid=" + key + "&units=imperial";
+    
+    // City Name
+    console.log(city);
 
     $.ajax({url: call, success: function(response) {
+        // Date
+        console.log(unixDate(response.current.dt));
+
         // Weather Icon
         console.log(response.current.weather[0].icon);
 
@@ -43,6 +44,21 @@ function getWeather(coordinates) {
         console.log(response.current.uvi);
     }})
 
+}
+
+function renderCurrent(city, date) {
+    var currentCard = $("<div>"); // .attr("class", "container")
+
+    currentCard.append($("<h2>").text(city));
+    currentCard.append($("<p>").text("(" + date + ")"));
+
+    weatherInfo.append(currentCard);
+}
+
+function unixDate(timestamp) {
+    var date = new Date(timestamp * 1000);
+    var month = date.getMonth() + 1;
+    return month + "/" + date.getDate();
 }
 
 srchBtn.on("click", getCoor);
